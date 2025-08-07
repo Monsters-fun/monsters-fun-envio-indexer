b87960021662d6abbc801735d822cc8a4de84ca3
Mahmood Mapara <83840620+moodster2000@users.noreply.github.com>
2025-08-07 12:45:37 -0400

    constant changes

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
-export const WIN_POINTS_MULTIPLIER: number = 0.01;
-export const TRADE_POINTS_MULTIPLIER: number = 50;
-export const MONSTER_XP_MULTIPLIER: number = 2; 
+export const WIN_POINTS_MULTIPLIER: number = 5550;
+export const TRADE_POINTS_MULTIPLIER: number = 37600;
+export const MONSTER_XP_MULTIPLIER: number = 0.1;  
 
 export const TEN_TO_THE_17: bigint = 10n ** 17n;
 export const ZERO_ADDRESS: string = "0x0000000000000000000000000000000000000000";
